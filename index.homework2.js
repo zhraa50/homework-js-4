86454239ca28618b2365efbb1ca45d52bd9da1e0
unknown <zhra5825@gmail.com>
2022-06-16 07:13:48 +0300

    f

---
+++ index.homework2.js
@@ -0,0 +1,41 @@
+let cel=50;
+
+var celtofar= cel*9/5+32;
+var m = "   celsius to fahreheit  = " + celtofar;
+console.log(m);
+
+let feh =45;
+var fehtocel=feh+-32*5/9;
+var m = " fehrnihet to celius " +fehtocel;
+console.log(m);
+
+var Numbers = [23,54,32,87,47];
+var sum =0 ;
+ for (i =0 ;i>=Numbers.length ;i++){
+ sum += Numbers[i];
+
+ }
+ console.logs(sum);
+
+let Numbers = [23,54,32,87,47];
+
+for (i = 1; i < arr.length; i++) {
+    if (arr[i] > max)
+        max = arr[i];
+}
+
+return max
+
+let num = [1,2,3,4,5,6,7,8,9,10,];
+let reverseArr = [];
+
+for (let i = 0; i <= num.length - 1; i++) {
+   reverseArr.unshift(num[i]);
+
+}
+
+console.log(reverseArr);
+
+for (i = 0; i <= 5; i++) {
+  console.log("*".repeat(i));
+}
